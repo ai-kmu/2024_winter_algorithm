90b24fd4562ea807d6511b1b70e18a728f555dd0
chkim417 <51479663+chkim417@users.noreply.github.com>
2024-01-23 16:16:32 +0900

    Update chaehwan_2.py

--- Algorithm/Greedy/chaehwan_2.py
+++ Algorithm/Greedy/chaehwan_2.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def eraseOverlapIntervals(self, intervals):
+        # x[1]만 해도 되는데 정렬 기준이 2개라면 이렇게 쓴다는 걸 알려주기 위해서^^
         intervals.sort(key=lambda x: (x[1], x[0]))
         last, cnt = -5 * 10**4, 0
         for interval in intervals:
