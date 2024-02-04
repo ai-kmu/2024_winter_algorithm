3dc34da9db16fa1493c0ca84be0ca66933aa0af0
chkim417 <51479663+chkim417@users.noreply.github.com>
2024-02-04 18:06:19 +0900

    Update 1.py

--- Algorithm/DFS, BFS/chaehwan/1.py
+++ Algorithm/DFS, BFS/chaehwan/1.py
@@ -14,7 +14,7 @@ def sumRelay(upperSum,node):
 class Solution(object):
     def sumNumbers(self, root):return sumRelay(0, root)
 
-
+2번 풀이, 내려갈 때마다 문자열이 "4", "49", "495"로 변하고 leaf이면 int형으로 바꿔서 finals에 저장 후, 다 돈 다음에 finals 합치기
 # class Solution(object):
 #     def sumNumbers(self, root):
 
@@ -22,15 +22,13 @@ class Solution(object):
 #         def untilLeaf(node, relay):
 #             relay += str(node.val)
 #             if node.left is None and node.right is None:
-#                 finals.append(relay)
+#                 finals.append(int(relay))
 #             if node.left is not None:
 #                 untilLeaf(node.left, relay)
 #             if node.right is not None:
 #                 untilLeaf(node.right, relay)
 
 #         untilLeaf(root, "")
-#         res = 0
-#         for x in finals:
-#             relay += int(x)
+#         res = sum(finals)
 #         return res
         
