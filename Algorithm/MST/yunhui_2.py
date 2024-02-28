1e9692439fa8bf0487983fce86246b3f0e286725
YunHui Kim <106079352+kyunhui@users.noreply.github.com>
2024-02-28 13:59:54 +0000

    Create yunhui_2.py

---
+++ Algorithm/MST/yunhui_2.py
@@ -0,0 +1,25 @@
+class UnionFind:
+    def __init__(self, n):
+        self.p = list(range(n))
+        self.n = n
+    
+    def union(self, a, b):
+        if self.find(a) == self.find(b):
+            return False
+        
+        self.p[self.find(a)] = self.find(b)
+        self.n -= 1
+        return True
+    
+    def find(self, x):
+        if self.p[x] != x:
+            self.p[x] = self.find(self.p[x])
+        return self.p[x]
+    
+class Solution:
+    def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        for i, e, in enumerate(edges):
+            e.append(i)
+        
+        edges.sort(key=lambda x: x[2])
+
