d3545639d69c4a32d2db3010c7751f0cb3989861
Chaewon Shin <84088060+mirageciel@users.noreply.github.com>
2024-03-01 14:27:47 +0900

    Create chaewon2.py

---
+++ Algorithm/MST/chaewon2.py
@@ -0,0 +1,35 @@
+# 미완성 코드
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+
+    def find(self, x):
+        if self.parent[x] == x:
+            return x
+        self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a == root_b:
+            return False
+        if self.rank[root_a] < self.rank[root_b]:
+            self.parent[root_a] = root_b
+        elif self.rank[root_a] > self.rank[root_b]:
+            self.parent[root_b] = root_a
+        else:
+            self.parent[root_b] = root_a
+            self.rank[root_a] += 1
+        return True
+
+class Solution:
+    def findCriticalAndPseudoCriticalEdges(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        critical_edges = []
+        pseudo_critical_edges = []
+
+        # 미완성
+      
+        return [critical_edges, pseudo_critical_edges]
