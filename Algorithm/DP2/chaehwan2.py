2aadfd61f0d24b0b5d7ed4e5ff24d14337bcc64d
chkim417 <51479663+chkim417@users.noreply.github.com>
2024-03-13 11:18:15 +0900

    Create chaehwan2.py

---
+++ Algorithm/DP2/chaehwan2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        # 2차원dp
+        dp = [[0] * n for _ in range(n)]
+
+
+        for i in range(n):
+            dp[i][i] = 1
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+
+        return dp[0][n - 1]
